7d0052938730aa3e6ce455c6adc3e13ba1b03347
LinkyPi <trouble.linky@gmail.com>
2021-12-18 09:47:55 +0800

    1. 增加二叉树节点间最大距离 2. 增加找零问题 CoinValueMulti 3. 增加马拉车算法 4. 增加对指定窗口大小的最大值数组 5. 增加 N 皇后问题 6. 增加最大异或和 7. 增加母牛生仔问题 8. 增加斐波那契数应用案例-求达标字符串数量

---
+++ src/com/lynch/extern/MaxWindow.java
@@ -0,0 +1,50 @@
+package com.lynch.extern;
+
+import com.lynch.tools.Utils;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个数组, 按顺序求指定窗口大小的最大值数组
+ */
+public class MaxWindow {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 3, 5, 4, 3, 3, 6, 7};
+        int[] result = getMaxWindow(arr, 3);
+        Utils.printArr(result);
+    }
+
+    private static int[] getMaxWindow(int[] arr, int windowSize) {
+        int[] maxArr = new int[arr.length - windowSize + 1];
+        // 使用双端队列记录从左到右按顺序以此递减的序列
+        Deque<Integer> queue = new LinkedList<>();
+        int right = 0, left = 0;
+        while (right < arr.length) {
+            if (queue.size() == 0) {
+                queue.offer(right);
+                right++;
+                continue;
+            }
+
+            int current = arr[right];
+            // 若后面加入的数与单调序列的最大值相等或较大大,则依次移除尾部元素后再将新元素放入队列
+            while (queue.size() > 0 && current >= arr[queue.getLast()]) {
+                queue.removeLast();
+            }
+            queue.offer(right);
+            // 若已经达到窗口最大值 则输出队列头部的元素到最大值数组
+            if (right - left + 1 == windowSize) {
+                maxArr[left] = arr[queue.peekFirst().intValue()];
+                left++;
+                // 滑动窗口不断右移, 同时删除旧的元素位置
+                if (left > queue.getFirst()) {
+                    queue.removeFirst();
+                }
+            }
+
+            right++;
+        }
+        return maxArr;
+    }
+}
